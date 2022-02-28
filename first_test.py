17bb4671e7e62f09e20ff9089029b149daef605f
Jun Xiong <xjurn@aliyun.com>
2022-02-28 10:00:42 +0000

    adding predictor of the file

--- first_test.py
+++ first_test.py
@@ -1,5 +1,7 @@
 # coding=utf8
 
 from speechbrain.pretrained import EncoderDecoderASR
+import ipdb; ipdb.set_trace()
 asr_model = EncoderDecoderASR.from_hparams(source="ddwkim/asr-conformer-transformerlm-ksponspeech", savedir="pretrained_models/asr-conformer-transformerlm-ksponspeech",  run_opts={"device":"cuda"})
-asr_model.transcribe_file("ddwkim/asr-conformer-transformerlm-ksponspeech/record_0_16k.wav") 
+res = asr_model.transcribe_file("ddwkim/asr-conformer-transformerlm-ksponspeech/record_0_16k.wav") 
+print("res is:", res)
